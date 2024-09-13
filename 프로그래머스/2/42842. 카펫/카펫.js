e0a1374aa36728f8d4fba24054fb44d783c2df4f
JK <74600256+Programmer-JK@users.noreply.github.com>
2024-09-14 00:06:00 +0900

    [level 2] Title: 카펫, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -0,0 +1,11 @@
+function solution(brown, yellow) {
+    var answer = [];
+    let temp = (brown-4)/2
+    for(let i=1;i<temp;i++){
+        if(i*(temp-i) === yellow) {
+            answer = [temp-i+2, i+2];
+            break;
+        }
+    }
+    return answer;
+}
