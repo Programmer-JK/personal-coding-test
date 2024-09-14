1be60765f6c3430ac4b61ebaa26835646d34cd28
JK <74600256+Programmer-JK@users.noreply.github.com>
2024-09-14 13:56:56 +0900

    [level 2] Title: 구명보트, Time: 13.31 ms, Memory: 38.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -0,0 +1,17 @@
+function solution(people, limit) {
+    var answer = 0;
+    //오름차순 정렬
+    let sortPeople = people.sort((a,b) => a-b);
+    let i = 0;
+    let k = sortPeople.length -1;
+    while(i <= k) {
+        if(sortPeople[i]+sortPeople[k] <= limit) {
+            i++;
+            k--;
+        } else {
+            k--;
+        }
+        answer++;
+    }
+    return answer;
+}
