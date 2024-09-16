8f46ae50ed6178dab87eb3a20c550ce414754f1b
JK <74600256+Programmer-JK@users.noreply.github.com>
2024-09-16 13:17:51 +0900

    [level 3] Title: 네트워크, Time: 0.63 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,21 +1,4 @@
 function solution(n, computers) {
-
     var answer = 0;
-    let visited = new Array(n).fill(0);
-    for(let i=0;i<n;i++){
-        if(visited[i] == 1) continue;
-        answer++;
-        dfs(computers, visited, i);
-    }
-    
     return answer;
-}
-
-function dfs(table, visited, node) {
-    visited[node] = 1;
-    for(let i=0;i<table.length;i++){
-        if(table[node][i] && visited[i] == 0) {
-            dfs(table, visited, i);
-        }
-    }
 }
