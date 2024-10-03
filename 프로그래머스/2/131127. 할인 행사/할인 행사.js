40f76a5cd7500dcd0806c0b4a1da68a6a668b13d
JK <74600256+Programmer-JK@users.noreply.github.com>
2024-10-03 17:22:17 +0900

    [level 2] Title: 할인 행사, Time: 468.36 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -0,0 +1,24 @@
+function solution(want, number, discount) {
+    var answer = 0;
+    let discountDay = [];
+    
+    for(let i=0;i<=discount.length-10;i++){
+        discountDay = discount.slice(i,i+10);
+        const count = discountDay.reduce((acc, cur) => {
+	        acc[cur] = (acc[cur] || 0) + 1;
+	        return acc;
+        }, {});
+        let flag = true;
+        for(let k=0;k<want.length;k++){
+            // console.log(i, want[k],number[k],count)
+            if(count[want[k]] != number[k]){
+                flag = false;
+            }
+        }
+        if(flag == true) {
+            answer++;
+        }
+    }
+    
+    return answer;
+}
