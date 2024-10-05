879ae98e28ed06f8cb00927d6a59c0900ad6d599
JK <74600256+Programmer-JK@users.noreply.github.com>
2024-10-05 21:47:19 +0900

    [level 2] Title: 타겟 넘버, Time: 14.90 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,27 +1,8 @@
 function solution(numbers, target) {
     var answer = 0;
-    let visited = new Array(numbers.length).fill(false);
     const bfs = (visited, num) => {
-        // console.log(visited, num)
-        // let visit = [...visited];
-        // if(visit.indexOf(false) == -1){
-        //     // console.log(visited, num)
-        //     if(num == target){
-        //         console.log(visit, num)
-        //         answer++;
-        //     }
-        //     return;
-        // }
-        // for(let i=0;i<numbers.length;i++){
-        //     if(!visit[i]){
-        //         visit[i] = true;
-        //         bfs(visit, num+numbers[i]);
-        //         bfs(visit, num-numbers[i]);
-        //     } 
-        // }
         let step = visited;
         if(step == numbers.length){
-            // console.log(visited, num)
             if(num == target){
                 answer++;
             }
@@ -31,6 +12,5 @@ function solution(numbers, target) {
         bfs(step+1, num-numbers[step]);
     }
     bfs(0, 0);
-    console.log(answer);
     return answer;
 }
