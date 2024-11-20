b0150a61f762e4f516c8044a174ada373e4cf3be
JK <74600256+Programmer-JK@users.noreply.github.com>
2024-11-21 01:57:21 +0900

    [level 2] Title: 타겟 넘버, Time: 14.38 ms, Memory: 35.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,13 +1,13 @@
 function solution(numbers, target) {
-    var answer = 0;
-    const dfs = (num, sum) => {
-        if(num == numbers.length && sum == target) answer++;
-        if(num > numbers.length-1) return;
-        let minusSum = sum-numbers[num];
-        let plusSum = sum+numbers[num];
-        dfs(num+1, minusSum);
-        dfs(num+1, plusSum);
-    };
-    dfs(0, 0);
+    let answer = 0;
+    const dfs = (num, index)=>{
+        if(index == numbers.length) {
+            if(num == target) answer++;
+            return;
+        }
+        dfs(num+numbers[index], index+1)
+        dfs(num-numbers[index], index+1)
+    }
+    dfs(0,0);
     return answer;
 }
