f59ec41e753be1ff1ad06db57168a4819f1cb7d4
JK <74600256+Programmer-JK@users.noreply.github.com>
2024-11-21 00:01:52 +0900

    [level 2] Title: [1차] 캐시, Time: 34.01 ms, Memory: 41.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -0,0 +1,28 @@
+function solution(cacheSize, cities) {
+    var answer = 0;
+    let cacheMemory = new Array(cacheSize);
+    let cacheHit = 1;
+    let cacheMiss = 5;
+    
+    if(cacheSize===0) {
+        return cacheMiss*cities.length;
+    }
+    
+    for(let cityIndex=0;cityIndex<cities.length;cityIndex++) {
+        const city = cities[cityIndex].toUpperCase();
+        if(cacheMemory.includes(city)){
+            let curIndex = cacheMemory.indexOf(city);
+            cacheMemory.splice(curIndex,1);
+            cacheMemory.push(city);
+            answer+=cacheHit;
+        } else {
+            if(cacheMemory.length === cacheSize){
+                cacheMemory.shift();   
+            }
+            cacheMemory.push(city);
+            answer+=cacheMiss;
+        }
+    }
+    
+    return answer;
+}
