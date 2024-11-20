25d2c1cd7e97e32fff644226bb8b87e5f78d5f4c
JK <74600256+Programmer-JK@users.noreply.github.com>
2024-11-20 23:34:25 +0900

    [level 2] Title: 할인 행사, Time: 78.24 ms, Memory: 43.1 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,24 +1,30 @@
 function solution(want, number, discount) {
-    var answer = 0;
-    let discountDay = [];
-    
-    for(let i=0;i<=discount.length-10;i++){
-        discountDay = discount.slice(i,i+10);
-        const count = discountDay.reduce((acc, cur) => {
-	        acc[cur] = (acc[cur] || 0) + 1;
-	        return acc;
-        }, {});
-        let flag = true;
-        for(let k=0;k<want.length;k++){
-            // console.log(i, want[k],number[k],count)
-            if(count[want[k]] != number[k]){
-                flag = false;
-            }
-        }
-        if(flag == true) {
-            answer++;
-        }
+    let answer = 0;
+    let buyMap = new Map();
+    let totalWantCount = 0;
+    for(let i=0;i<want.length;i++){
+        totalWantCount+=number[i];
     }
     
+    for(let startDay=0;startDay<discount.length;startDay++){
+        let discountList = discount.slice(startDay, startDay+totalWantCount);
+        buyMap.clear();
+        for(let i=0;i<discountList.length;i++){
+            if(buyMap.has(discountList[i])){
+                buyMap.set(discountList[i], buyMap.get(discountList[i])+1);
+            } else {
+                buyMap.set(discountList[i], 1);
+            }
+        }
+        let keyArr = [...buyMap.keys()];
+        let checkCount = 0;
+        // console.log(keyArr);
+        for(let i=0;i<want.length;i++){
+            if(buyMap.has(want[i]) && buyMap.get(want[i]) === number[i]){
+                checkCount++;
+            }
+        }
+        if(checkCount === number.length) answer++;
+    }
     return answer;
 }
