f150e35cdce13ea91d60136f34a9b3a5ccb20a99
JK <74600256+Programmer-JK@users.noreply.github.com>
2024-11-20 23:44:11 +0900

    [level 2] Title: 할인 행사, Time: 70.03 ms, Memory: 43.2 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,10 +1,7 @@
 function solution(want, number, discount) {
     let answer = 0;
     let buyMap = new Map();
-    let totalWantCount = 0;
-    for(let i=0;i<want.length;i++){
-        totalWantCount+=number[i];
-    }
+    let totalWantCount = number.reduce((acc, cur)=>acc+cur, 0);
     
     for(let startDay=0;startDay<discount.length;startDay++){
         let discountList = discount.slice(startDay, startDay+totalWantCount);
@@ -16,9 +13,7 @@ function solution(want, number, discount) {
                 buyMap.set(discountList[i], 1);
             }
         }
-        let keyArr = [...buyMap.keys()];
         let checkCount = 0;
-        // console.log(keyArr);
         for(let i=0;i<want.length;i++){
             if(buyMap.has(want[i]) && buyMap.get(want[i]) === number[i]){
                 checkCount++;
