3847fbd3d02fd1f33016c03180dbdc59647feb6b
JK <74600256+Programmer-JK@users.noreply.github.com>
2025-01-02 22:49:08 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.74 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,13 +1,10 @@
 function solution(A,B){
     var answer = 0;
-
-    A.sort((a,b)=>(a-b));
-    
-    B.sort((a,b)=>(b-a));
-    
-    for(var i=0;i<A.length;i++){
-        answer+=A[i]*B[i];
+    A.sort((a,b) => (a-b));
+    B.sort((a,b) => (b-a));
+    for(let i=0;i<A.length;i++){
+        answer += A[i]*B[i];
     }
-    
+
     return answer;
 }
