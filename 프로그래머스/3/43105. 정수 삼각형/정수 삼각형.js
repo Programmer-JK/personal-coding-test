f7af25aee3b1253dbcf03344c118c7987eec48ec
JK <74600256+Programmer-JK@users.noreply.github.com>
2025-01-03 21:33:36 +0900

    [level 3] Title: 정수 삼각형, Time: 4.90 ms, Memory: 41.2 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,8 +1,13 @@
 function solution(triangle) {
-    var answer = 0;
-    for(let i=triangle.length-1;i>0;i--){
+    let answer = 0;
+    const len = triangle.length;
+    for(let i=len-1;i>0;i--) {
         for(let k=0;k<triangle[i].length-1;k++){
-            triangle[i-1][k] += Math.max(triangle[i][k], triangle[i][k+1]);
+            if(triangle[i][k] > triangle[i][k+1]){
+                triangle[i-1][k] += triangle[i][k];
+            } else {
+                triangle[i-1][k] += triangle[i][k+1];
+            }
         }
     }
     return triangle[0][0];
