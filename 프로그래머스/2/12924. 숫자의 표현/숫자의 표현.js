477921117f239009ee7f6207e13ce7beaec1fbd0
JK <74600256+Programmer-JK@users.noreply.github.com>
2025-01-04 20:56:49 +0900

    [level 2] Title: 숫자의 표현, Time: 2.39 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,11 +1,17 @@
 function solution(n) {
-    var answer = 0;
-    for(var i=1;i<=n;i++){
-        var sum = i;
-        for(var k=i+1;sum<n;k++){
-            sum+=k;
+    let answer = 0;
+    const max = Math.ceil(n/2);
+    for(let i=1;i<=max;i++) {
+        let sum = 0;
+        let add_num = i;
+        while(sum <= n){
+            sum+=add_num;
+            if(sum === n) {
+                answer++;
+            } else {
+                add_num++;
+            }
         }
-        if(sum===n) answer++;
     }
-    return answer;
+    return n==1? 1 : answer+1;
 }
