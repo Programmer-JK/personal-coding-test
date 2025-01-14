cbc3817b40788f95ca7baeea4fea39da23e22406
JK <74600256+Programmer-JK@users.noreply.github.com>
2025-01-14 16:01:54 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,17 +1,17 @@
 function solution(n) {
-    var answer = n;
-    while(true) {
-        answer++;
-        if(countOne(n) === countOne(answer)) break;
-    }
-    return answer;
+  let answer = n;
+  while (true) {
+    answer++;
+    if (countOne(n) === countOne(answer)) break;
+  }
+  return answer;
 }
 
-function countOne(n){
-    var count=0;
-    let temp = n.toString(2);
-    for(let i=0;i<temp.length;i++){
-        if(temp[i]==='1') count++;
-    }
-    return count;
+function countOne(n) {
+  let count = 0;
+  let temp = n.toString(2);
+  for (let i = 0; i < temp.length; i++) {
+    if (temp[i] === "1") count++;
+  }
+  return count;
 }
