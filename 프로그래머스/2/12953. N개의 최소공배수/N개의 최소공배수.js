ffc2a2e9a4f5d7d9c136bbead23e2c420d8a09f8
JK <74600256+Programmer-JK@users.noreply.github.com>
2025-01-14 17:48:21 +0900

    [level 2] Title: N개의 최소공배수, Time: 4.75 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,18 +1,18 @@
+function gcd(a,b) {
+    let mod = a%b;
+    if(mod === 0) return b;
+    return gcd(b, mod);
+}
+
 function solution(arr) {
-    var answer = arr[arr.length-1];
-    let num = 1;
-    while(true) {
-        let number = answer*num;
-        let count = 0;
-        arr.forEach(e=>{
-            if(number%e == 0) count++; 
-        })
-        if(count == arr.length) {
-            answer = number;
-            break;  
-        } else {
-            num++;
-        }
+    var answer = 0;
+    let temp = [];
+    temp.push(arr[0]);
+    for(let i=1;i<arr.length;i++){
+        let a = temp.pop();
+        let gcdNum = gcd(a, arr[i]);
+        temp.push((a/gcdNum)*(arr[i]/gcdNum)*gcdNum);
     }
-    return answer;
+    console.log(temp)
+    return temp[0];
 }
