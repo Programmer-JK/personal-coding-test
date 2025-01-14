5fcac6b75d02ad8e5b2a731a49901a529245b505
JK <74600256+Programmer-JK@users.noreply.github.com>
2025-01-14 16:11:12 +0900

    [level 2] Title: 피보나치 수, Time: 5.53 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -1,13 +1,8 @@
-function F(n) {
-    if(n < 2) return n;
-    else return F(n-1)+F(n-2);
-}
-
 function solution(n) {
     var answer = 0;
-    var num = [0,1];
-    for(let i=2;i<n+1;i++){
-        num[i] = ((num[i-1] + num[i-2]) %1234567);
+    let arr = [0,1];
+    for(let i=2;i<=n;i++){
+        arr[i] = (arr[i-1]+arr[i-2])%1234567;
     }
-    return num[n];
+    return arr[n];
 }
