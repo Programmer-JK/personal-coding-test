887bfb944122ab706d66c0732259174f0acfad95
JK <74600256+Programmer-JK@users.noreply.github.com>
2025-01-14 20:07:43 +0900

    [level 3] Title: 야근 지수, Time: 151.20 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.js
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.js
@@ -2,72 +2,54 @@ class maxHeap {
     constructor() {
         this.heap = [];
     }
-    
     enqueue(v) {
         this.heap.push(v);
-        let index = this.heap.length -1;
-        while(index > 0) {
-            const parentIndex = Math.floor((index-1)/2);
-            if(this.heap[parentIndex] >= this.heap[index]) break;
-            [this.heap[parentIndex], this.heap[index]] = [this.heap[index], this.heap[parentIndex]];
-            index = parentIndex;
-        } 
+        let idx = this.heap.length -1;
+        while(idx>0) {
+            let parentIdx = Math.floor((idx-1)/2);
+            if(this.heap[parentIdx] > this.heap[idx]) return;
+            [this.heap[parentIdx], this.heap[idx]] = [this.heap[idx], this.heap[parentIdx]];
+            idx = parentIdx;
+        }
     }
-    
     dequeue() {
-        if(this.heap.length === 1) return this.heap.pop();
         const max = this.heap[0];
         this.heap[0] = this.heap.pop();
-        let index = 0;
+        let idx = 0;
         while(true) {
-            let big = index;
-            const left = 2*index+1;
-            const right = 2*index+2;
-            
+            if(this.heap.length === 1) return this.heap.pop();
+            let big = idx;
+            let left = idx*2+1;
+            let right = idx*2+2;
             if(left < this.heap.length && this.heap[left] > this.heap[big]) {
                 big = left;
             }
             if(right < this.heap.length && this.heap[right] > this.heap[big]) {
                 big = right;
             }
-            
-            if(big === index) break;
-            [this.heap[index], this.heap[big]] = [this.heap[big], this.heap[index]];
-            index = big;
+            if(big === idx) break;
+            [this.heap[idx], this.heap[big]] = [this.heap[big], this.heap[idx]];
+            idx = big;
         }
         return max;
+    }
+    getArr() {
+        return this.heap;
     }
 }
 
 function solution(n, works) {
     var answer = 0;
-    let max = new maxHeap();
-    
-    works.forEach(e => max.enqueue(e));
-    // console.log(max.heap)
-    
-    for(let i=0;i<n;i++){
-        if(max.heap[0] >0) {
-            let a = max.dequeue();
-            a--;
-            max.enqueue(a);
-        } else {
-            break;
-        }
+    let heap = new maxHeap();
+    for(let i=0;i<works.length; i++) {
+        heap.enqueue(works[i]);
+    };
+    while(n>0) {
+        let work = heap.dequeue();
+        if(work <= 0) break;
+        heap.enqueue(work-1);
+        n--;
     }
-    
-//     for(let i=0;i<n;i++){
-//         temp = temp.sort((a,b)=>b-a);
-//         if(temp[0] > 0){
-//             temp[0]--;
-//         } else {
-//             break;
-//         }
-//     }
-    
-    for(let k=0;k<max.heap.length;k++){
-        answer += max.heap[k] * max.heap[k]
-    }
-    
+    answer = n>0 ? 0 : heap.heap.reduce((acc, work)=> acc+ work*work, 0);
     return answer;
 }
